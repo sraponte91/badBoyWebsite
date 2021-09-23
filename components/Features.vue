07b344a998661deef72d420dfd33c099b3c5e29a
Samael Aponte <samaelaponte@akimbo.com.co>
2021-09-23 12:52:27 -0400

    firstCommit

---
+++ components/Features.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="feature_service_area text-center">
+    <div class="feature_service_box_width">
+      <i class="icon-wallet"></i>
+      <p>No Credit Needed,<br> We Finance</p>
+    </div><!--end .feature_service_box_width-->
+    <div class="feature_service_box_width">
+      <i class="icon-diploma"></i>
+      <p>We work with <br> Certified Material</p>
+    </div><!--end .feature_service_box_width-->
+    <div class="feature_service_box_width">
+      <i class="icon-big_clock"></i>
+      <p>Faster and Quialified Service</p>
+    </div><!--end .feature_service_box_width-->
+    <div class="feature_service_box_width">
+      <i class="icon-handshake"></i><p>We have everything in house</p>
+      <p></p>
+    </div><!--end .feature_service_box_width-->
+    <div class="feature_service_box_width">
+      <i class="icon-mechanic"></i>
+      <p>More than 10 years experience</p>
+    </div><!--end .feature_service_box_width-->
+  </div>
+</template>
+
+<script>
+    export default {
+      name: "Features"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <Header />
+    <nuxt />
+    <Footer />
+  </div>
+</template>
+
+<script>
+  import Header from "~/components/Header";
+  import Footer from "~/components/Footer";
+  export default {
+    components: {
+      Footer,
+      Header
+    },
+    head(){
+      return {
+        title: "Bad Boy Custom - Everything for Advertisement -"
+      }
+    },
+    mounted () {
+      this.$nextTick(() => {
+        this.$nuxt.$loading.start()
+        setTimeout(() => this.$nuxt.$loading.finish(), 500)
+      })
+    }
+  }
+</script>
+<style>
+
+</style>
